2f5a9f2a54ea7cfd55545e7582d3f6d6d6db9513
Abhi7230 <abhir0609@gmail.com>
2024-07-27 02:21:20 +0530

    Describe your changes

--- compiler/app.js
+++ compiler/app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: 'https://frontend-last-tau.vercel.app/', 
     credentials: true, 
 };
 
